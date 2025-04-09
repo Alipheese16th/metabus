9d9f7578cb500c8dc7bd4601504b9b48978c3aa5
chiyun.shin <shwoo1103@gmail.com>
2025-04-09 16:54:43 +0900

    Auto push 2025-04-09 16:54:43.50

--- MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a013eee6a84e3c0cef6a8611c4db9b4206dd77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e71af1a9736fa9d34bc994b7ee1af9f62462fe2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
