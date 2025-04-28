03eba4c18fe47bbda4387c34f109e94e268eb78d
chiyun.shin <shwoo1103@gmail.com>
2025-04-28 09:07:51 +0900

    Git Push 2025-04-28  9:07:51.07

--- MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
+++ MyCSharpApp/obj/Debug/net9.0/MyCSharpApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+babd4bcde32c5b3befde49c5c94b84b7afdc06a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37cf9326d1704022f00d812bca9400868d124fa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyCSharpApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ unity/Cshop_study/Assets/02.Scripts/testtest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class testtest : MonoBehaviour
+{
+
+    public delegate void TestDelegateA(); // 델리게이트를 명시할때 리턴타입과 파라미터를 미리 정해둔다
+    public delegate int TestDelegateB(int n); 
+
+    void Start()
+    {
+
+        TestDelegateA dele1, dele11, dele111; // 얜 void 함수이고 파라미터가 없는 경우의 함수만 저장이 가능함
+        TestDelegateB dele2; 
+
+        dele1 = testFunctionA;
+        dele11 = testFunctionC;
+        dele111 = dele1 + dele11; // 순선대로 실행됨
+        dele2 = testFunctionB;
+
+        dele111 -= dele11; // 빼면 그 함수는 실행안됨
+        dele111 -= dele11;
+        dele111();
+
+
+        
+    }
+
+
+    void testFunctionA() {
+        Debug.Log("Test Delegate A !!");
+    }
+    
+    int testFunctionB(int n) {
+        return n * n;
+    }
+
+    void testFunctionC() {
+        Debug.Log("Hello Unity !!");
+    }
+
+}
