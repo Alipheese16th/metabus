27e4a91a9fc722a7a9c262bbd6a88045bc0e4ea7
chiyun.shin <shwoo1103@gmail.com>
2025-04-30 21:06:21 +0900

    Git Push 2025-04-30 21:06:21.20

--- MyCSharpApp/Program.cs
+++ MyCSharpApp/Program.cs
@@ -12,30 +12,41 @@ try
     }
 
     Queue<int> que = new Queue<int>();
+    int lastInput = 0;
 
     for (int i = 0; i < N; i++)
     {
         string[] input = (sr.ReadLine() ?? "").Split();
         string keyword = input[0];
+
         switch (keyword)
         {
             case "push":
+            que.Enqueue(int.Parse(input[1]));
+            lastInput = int.Parse(input[1]);
             break;
             case "pop":
+            int pop = que.Count == 0 ? -1 : que.Dequeue();
+            sw.WriteLine(pop);
             break;
             case "size":
+            sw.WriteLine(que.Count);
             break;
             case "empty":
+            int isEmpty = que.Count == 0 ? 1 : 0;
+            sw.WriteLine(isEmpty);
             break;
             case "front":
+            int first = que.Count == 0 ? -1 : que.Peek();
+            sw.WriteLine(first);
             break;
             case "back":
+            int last = que.Count == 0 ? -1 : lastInput;
+            sw.WriteLine(last);
             break;
         }
 
-
     }
-
 
 
 
