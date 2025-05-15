948acc427beaeaaba0aeb09f8b8b78c1578a8caf
chiyun.shin <shwoo1103@gmail.com>
2025-05-15 19:23:08 +0900

    Git Push 2025-05-15 19:23:07.93

---
+++ unity/SpaceShooter2025/Assets/02.Scripts/FollowCam.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    public float moveDamping = 15f;
+    public float rotDamping = 10f;
+    public float distance = 3f;
+    public float height = 2.5f;
+    public float targetOffset = 2.0f;
+    //public float smoothTime = 0.2f;
+    //private Vector3 velocity = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        var camPos = target.position - (target.forward * distance) + (target.up * height); // 카메라 위치값 구함
+        transform.position = Vector3.Slerp(transform.position, camPos, Time.deltaTime * moveDamping); // 카메라 이동
+        transform.rotation = Quaternion.Slerp(transform.rotation, target.rotation, Time.deltaTime * rotDamping); // 카메라 회전
+        //transform.position = Vector3.SmoothDamp(
+        //    transform.position,      // 현재 위치
+        //    camPos,         // 목표 위치
+        //    ref velocity,            // 현재 속도 (반드시 ref)
+        //    smoothTime               // 감속 시간
+        //);
+        transform.LookAt(target.position + (target.up * targetOffset)); // 카메라 보는 방향 각도 조절
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(target.position + (target.up * targetOffset), 0.1f);
+        Gizmos.DrawLine(transform.position, target.position + (target.up * targetOffset));
+    }
+
+}
--- unity/SpaceShooter2025/Assets/02.Scripts/PlayerCtrl.cs
+++ unity/SpaceShooter2025/Assets/02.Scripts/PlayerCtrl.cs
@@ -2,35 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class PlayerAnim
+{
+    public AnimationClip idle;
+    public AnimationClip runF;
+    public AnimationClip runB;
+    public AnimationClip runL;
+    public AnimationClip runR;
+}
+
 public class PlayerCtrl : MonoBehaviour
 {
     private float h = 0.0f;
     private float v = 0.0f;
     private Transform tr;
     public float moveSpeed = 10f;
+    private Vector3 dir;
+    private float r;
+    public float rotationSpeed = 300f;
+    public PlayerAnim playerAnim;
+    public Animation anim;
 
     // Start is called before the first frame update
     void Start()
     {
         tr = transform;
+        anim = GetComponent<Animation>();
+        anim.clip = playerAnim.idle;
+        anim.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // update 라이프사이클에서 객체를 계속 새로 만드는 코드를 작성하면 메모리를 계속 할당하기때문에 비효율적임.
+        // new Vector3() 이런식으로 이동시키는거보다 Vector3.right 같은 정적(static) 속성을 사용하는게 훨씬 최적화가 좋다
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        r = Input.GetAxis("Mouse X");
+
+        dir = (Vector3.right * h) + (Vector3.forward * v);
+        dir = dir.sqrMagnitude > 1 ? dir.normalized : dir;
+
+        tr.Translate(dir * Time.deltaTime * moveSpeed);
+        //tr.position += new Vector3(h, 0, v) * moveSpeed * Time.deltaTime;
+
+        tr.Rotate(Vector3.up * r * Time.deltaTime * rotationSpeed);
+
+        if (v > 0.1f)
+        {
+            anim.CrossFade(playerAnim.runF.name, 0.3f);
+        } 
+        else if (v < -0.1f)
+        {
+            anim.CrossFade(playerAnim.runB.name, 0.3f);
+        }
+        else if (h > 0.1f)
+        {
+            anim.CrossFade(playerAnim.runR.name, 0.3f);
+        }
+        else if (h < -0.1f)
+        {
+            anim.CrossFade(playerAnim.runL.name, 0.3f);
+        }
+        else
+        {
+            anim.CrossFade(playerAnim.idle.name, 0.3f);
+        }
+
+
+
 
     }
 
     private void FixedUpdate()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
-
-        tr.Translate(h * moveSpeed * Time.deltaTime, 0, v * moveSpeed * Time.deltaTime);
-        
-
-        //tr.position += new Vector3(h, 0, v) * moveSpeed;
-
     }
 
 
